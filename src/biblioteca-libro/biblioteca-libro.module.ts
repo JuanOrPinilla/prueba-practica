8d9368d3ef2b2598493a0f220bbc8d4357ae37bb
Juan Orduz <juanorpinilla@gmail.com>
2025-05-15 19:12:19 -0500

    primeras colecciones

--- src/biblioteca-libro/biblioteca-libro.module.ts
+++ src/biblioteca-libro/biblioteca-libro.module.ts
@@ -1,9 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BibliotecaLibroService } from './biblioteca-libro.service';
 import { BibliotecaLibroController } from './biblioteca-libro.controller';
+import { BibliotecaEntity } from '../biblioteca/biblioteca.entity';
+import { LibroEntity } from '../libro/libro.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BibliotecaEntity, LibroEntity])],
   providers: [BibliotecaLibroService],
   controllers: [BibliotecaLibroController]
 })
